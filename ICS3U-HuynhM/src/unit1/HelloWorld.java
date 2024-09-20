f96aa0dfe9182f0c5fbad3e50406e112c4e1fb9b
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-09-20 11:11:18 -0400

    Adding ICS3U project with Hello World class

---
+++ ICS3U-HuynhM/src/unit1/HelloWorld.java
@@ -0,0 +1,23 @@
+package unit1;
+/**
+ * Description: This program prints Hello World, and a second statement. <br>
+ * Date: Sept. 19, 2024
+ * @author Myra Huynh
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.println("Hello World!!");
+		System.out.println("I miss Dani");
+		/*
+		 * This is a block comment.
+		 * Javadoc comment by slash double star
+		 * Block comment by slash star
+		 * Single line comment by double slash 
+		 */
+		
+	
+	}
+
+}
