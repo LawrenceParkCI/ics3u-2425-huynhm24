52a02365381c90e2f8f5692dbdb6e011eb7414fd
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-09-20 11:41:06 -0400

    This is my AboutMe class.

---
+++ ICS3U-HuynhM/src/unit1/AboutMe.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description: This program prints information about the author <br>
+ * Date: September 20, 2024
+ * @author Myra Huynh
+ */
+
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// This statement says my name, grade, teacher and school
+		System.out.println("My name is Myra Huynh.");
+		System.out.println("I am in Grade 11.");
+		System.out.println("My teacher is Ms. Kemp.");
+		System.out.println("I go to school at Lawrence Park Collegiate Institute");
+
+	}
+
+}
