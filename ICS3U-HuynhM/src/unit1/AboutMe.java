88d94b80a8a2879040f2d20895514eb39f163f47
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-09-23 09:44:55 -0400

    Added school quote and started schedule.

--- ICS3U-HuynhM/src/unit1/AboutMe.java
+++ ICS3U-HuynhM/src/unit1/AboutMe.java
@@ -12,7 +12,11 @@ public class AboutMe {
 		System.out.println("My name is Myra Huynh.");
 		System.out.println("I am in Grade 11.");
 		System.out.println("My teacher is Ms. Kemp.");
-		System.out.println("I go to school at Lawrence Park Collegiate Institute");
+		System.out.println("I go to school at Lawrence Park Collegiate Institute.");
+		// This statement is a cheer for my school
+		System.out.println("\"GET HYPED LP\"");
+		// This is my school schedule this week
+		System.out.println("%-15s%-15s%-15s%-15s%-15s");
 
 	}
 
