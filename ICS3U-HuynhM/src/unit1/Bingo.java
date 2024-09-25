5fbee1522949cbec84f5ab922da65e9a0b16607f
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-09-25 10:39:23 -0400

    Created a Bingo card example

--- ICS3U-HuynhM/src/unit1/Bingo.java
+++ ICS3U-HuynhM/src/unit1/Bingo.java
@@ -1,5 +1,7 @@
 /**
- * Description: This program describes the rules of Bingo, and provides an example of a Bingo card. 
+ * Description: This program describes the rules of Bingo, and provides an example of a Bingo card.
+ * Date: September 24, 2024
+ * @author Myra Huynh
  */
 package unit1;
 
@@ -14,6 +16,19 @@ public class Bingo {
 		System.out.println("4. If the number is located, it is marked off.");
 		System.out.println("5. Steps 1 to 4 are repeated until a player matches a BINGO pattern.");
 		System.out.println("6. The winning player yells \"Bingo!\"");
+		System.out.println("-----------------------------------");
+		// These statements create an example of a BINGO card
+		System.out.println("\nBINGO Card:");
+		System.out.println(" ______________________");
+		System.out.printf("|%-5s%-6s%-5s%-5s%-1s|", "B", "I", "N", "G", "O");
+		System.out.println("\n|----------------------|");
+		System.out.printf("|%-5s%-5s%-5s%-5s%-1s|", "12", "17", "31", "48", "74");
+		System.out.printf("\n|%-5s%-5s%-5s%-5s%-1s|", "4", "22", "42", "57", "66");
+		System.out.printf("\n|%-5s%-4s%-6s%-5s%-1s|", "9", "19", "FREE", "49", "73");
+		System.out.printf("\n|%-5s%-5s%-5s%-5s%-1s|", "10", "29", "32", "46", "70");
+		System.out.printf("\n|%-5s%-5s%-5s%-5s%-1s|", "5", "16", "45", "60", "68");
+		System.out.println("\n|______________________|");
+	
 
 	}
 
