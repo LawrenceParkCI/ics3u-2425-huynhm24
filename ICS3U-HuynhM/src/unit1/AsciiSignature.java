1ff9a44b41561285226f5b03c029b74863f9183c
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-09-26 11:38:18 -0400

    Name and picture printed in ASCII style.

---
+++ ICS3U-HuynhM/src/unit1/AsciiSignature.java
@@ -0,0 +1,43 @@
+/**
+ * Description: This program prints the author's name and a picture in ASCII style. 
+ * Date: September 26, 2024
+ * @author Myra Huynh
+ */
+package unit1;
+
+public class AsciiSignature {
+
+	public static void main(String[] args) {
+		// This prints my name in ASCII cursive
+		System.out.println("  ,____,        ,_  __,      /_             ,__,   /_");
+		System.out.println("_/ / / (__(_/__/ (_(_/(_   _/ (__(_/__(_/__/ / (__/ (_");
+		System.out.println("          _/_                         _/_             ");
+		System.out.println("         (/                          (/              ");
+		// This prints my name with flames
+		System.out.println("   *                          )                           ");
+		System.out.println(" (  `                      ( /(                        )  ");
+		System.out.println(" )\\))(   (     (       )   )\\())   (   (            ( /(  ");
+		System.out.println("((_)()\\  )\\ )  )(   ( /(  ((_)\\   ))\\  )\\ )   (     )\\()) ");
+		System.out.println("(_()((_)(()/( (()\\  )(_))  _((_) /((_)(()/(   )\\ ) ((_)\\  ");
+		System.out.println("|  \\/  | )(_)) ((_)((_)_  | || |(_))(  )(_)) _(_/( | |(_) ");
+		System.out.println("| |\\/| || || || '_|/ _` | | __ || || || || || ' \\))| ' \\  ");
+		System.out.println("|_|  |_| \\_, ||_|  \\__,_| |_||_| \\_,_| \\_, ||_||_| |_||_| ");
+		System.out.println("         |__/                          |__/              ");
+		// This prints my name using the print of the American Flag
+		System.out.println("\n :::=== ===  ::: === :::====  :::====       :::  === :::  === ::: === :::= === :::  ===");
+		System.out.println(" ::: === === ::: === :::  === :::  ===      :::  === :::  === ::: === :::===== :::  ===");
+		System.out.println(" === === ===  =====  =======  ========      ======== ===  ===  =====  ======== ========");
+		System.out.println(" ===     ===   ===   === ===  ===  ===      ===  === ===  ===   ===   === ==== ===  ===");
+		System.out.println(" ===     ===   ===   ===  === ===  ===      ===  ===  ======    ===   ===  === ===  ===");
+		// This prints a photo of a chocolate bar
+		System.out.println("\n    __________________,.............,");
+		System.out.println("   /_/_/_/_/_/_/_/_/,-',  ,. -,-,--/|");
+		System.out.println("  /_/_/_/_/_/_/_/,-' //  /-| / /--/ /");
+		System.out.println(" /_/_/_/_/_/_/,-' `-''--'  `' '--/ /");
+		System.out.println("/_/_/_/_/_/_,:................../ /");
+		System.out.println("|________,'                   hh|/");
+		System.out.println("         \"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"\"'");
+		
+	}
+
+}
