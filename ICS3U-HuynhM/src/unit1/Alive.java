46668b2580d9ec04df21a5f21b5b4dd183e64567
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-10-08 11:39:39 -0400

    Completed all parts of assignment 2.

--- ICS3U-HuynhM/src/unit1/Alive.java
+++ ICS3U-HuynhM/src/unit1/Alive.java
@@ -29,15 +29,17 @@ public class Alive {
 		System.out.println("\nEnter today's date:");
 		System.out.println("Year:");
 		year2 = sc.nextInt();
-		System.out.println("Month:");
+		System.out.println("Month (in number form):");
 		month2 = sc.nextInt();
 		System.out.println("Day:");
 		day2 = sc.nextInt();
 		
 		//Using user input to calculate their days alive and time slept
-		int daysAlive = (12 * (year2 - year1 - 1)) + ((12 - month1) + (12 - month2));
+		int daysAlive = (year2 - year1) * 365 + (month2 - month1) * 30 + (day2 - day1);
+		System.out.println("You have been alive for " + daysAlive +" days. ");
 		
-		
+		int sleep = daysAlive * 8;
+		System.out.println("You have slept for a total of " + sleep + " hours.");
 		
 		sc.close();
 		
