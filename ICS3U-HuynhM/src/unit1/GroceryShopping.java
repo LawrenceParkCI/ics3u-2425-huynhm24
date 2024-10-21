0a6794bea45befc28d8951ac105829c29a5635b9
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-10-21 10:16:09 -0400

    Worked on Final Assignment for Unit 1

--- ICS3U-HuynhM/src/unit1/GroceryShopping.java
+++ ICS3U-HuynhM/src/unit1/GroceryShopping.java
@@ -1,7 +1,9 @@
 package unit1;
+
 import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.text.DecimalFormat;
 
 /**
  * Description: This program provides costs and a receipt for a user's grocery shopping list. 
@@ -18,14 +20,18 @@ public class GroceryShopping {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("|| $$$ \\\\\\ ========== \"Ms. Huynh's Grocery\" ========== /// $$$ ||");
+		final double TAX_RATE = 0.13;
+		DecimalFormat money = new DecimalFormat("$#,###,###.00");
 		
+		System.out.println("|| $$$ \\\\\\ ========== \"Myra's Grocery\" ========== /// $$$ ||");
+		/*
 		//Creating variables
 		String item1, item2;
 		Double cost1, cost2;
 		int quantity1, quantity2;
 		
 		//Gathering user input on their items
+		
 		System.out.println("\nWelcome!");
 		System.out.print("What would you like to buy? ");
 		item1 = sc.nextLine();
@@ -39,30 +45,53 @@ public class GroceryShopping {
 		System.out.println("\nNext item:");
 		System.out.print("What would you like to buy? ");
 		item2 = sc.nextLine();
-		System.out.print("How much does it cost? ");
+		System.out.print("How much does it cost? $");
 		cost2 = sc.nextDouble();
 		System.out.print("How many are you buying? ");
 		quantity2 = sc.nextInt();
 		System.out.println("Thank you!");
+		*/
+		
+		String item1 = "Bread";
+		String item2 = "Ice Cream";
+		double cost1 = 10;
+		double cost2 = 15;
+		int quantity1 = 2;
+		int quantity2 = 1;
+		
 		
 		//Calculating total prices
 		double totalPrice1 = cost1 * quantity1;
+		double totalPrice2 = cost2 * quantity2;
+		double subtotal = totalPrice1 + totalPrice2; 
+		double tax = subtotal * TAX_RATE;
+		double total = subtotal + tax;
 		
 		//Printing their receipt
 		System.out.println("\nThis is your receipt!");
-		System.out.println("\nMs. Huynh's Grocery");
+		System.out.println("\n|-------------------------Myra's Grocery-----------------------|");
+		
 		
 		//Finding the date and time of printing
-		LocalDate date = LocalDate.now();
-		LocalTime time = LocalTime.now();
+		LocalDateTime date = LocalDateTime.now();
+		DateTimeFormatter timeF = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 		
-		System.out.println(date);
-		System.out.println(time);
+		System.out.printf("|%-62s|", timeF.format(date));
+		System.out.printf("\n|%-62s|", " ");
 		
 		//Printing the item information based on user input
-		System.out.printf("\n%-15s|%-15s|%-15s|%-15s", "Item", "Price", "Quant.", "Total Price");
-		System.out.println("\n-----------------------------------------------------------------");
-		System.out.printf("\n%-15s|%-5s%10s|%-15s|%-5s%10s", item1, "$", cost1 + " ", quantity1, "$", totalPrice1 + " ");
+		System.out.printf("\n|%-15s|%-15s|%-15s|%-14s|", "Item", "Price", "Quant.", "Total Price");
+		System.out.println("\n|--------------------------------------------------------------|");
+		System.out.printf("|%-15s|%-5s%10s|%15s|%-5s%9s|", item1, " $", money.format(cost1) + " ", quantity1 + " ", " $", money.format(totalPrice1) + " ");
+		System.out.printf("\n|%-15s|%-5s%10s|%15s|%-5s%9s|", item2, " $", money.format(cost2) + " ", quantity2+ " ", " $", money.format(totalPrice2) + " ");
+		System.out.printf("\n|%-62s|", " ");
+		System.out.printf("\n|%32s%15s:%-5s%9s|", " ", "Subtotal", " $", money.format(subtotal) + " ");
+		System.out.printf("\n|%32s%15s:%-5s%9s|", " ", "Tax", " $", money.format(tax) + " ");
+		System.out.printf("\n|%32s%15s:%-5s%9s|", " ", "Total", " $", money.format(total) + " ");
+		System.out.println("\n|______________________________________________________________|");
+		System.out.println("\n\nThis is approximately $" + Math.rint(total) + ".");
+		System.out.println("\nHave a good day!");
+		System.out.println("Thank you for shopping at Myra's Grocery!");
 		
 		
 		
