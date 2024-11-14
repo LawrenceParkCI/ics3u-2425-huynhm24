15f2a2ff79987e10aaed74f23af168e028df6028
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-11-14 11:33:04 -0500

    Completed String Challenge program

--- ICS3U-HuynhM/src/unit2/StringChallenge.java
+++ ICS3U-HuynhM/src/unit2/StringChallenge.java
@@ -25,7 +25,7 @@ public class StringChallenge {
 		Scanner sc = new Scanner(System.in);
 		
 		//Asking the user for input
-		System.out.println("Do you prefer watermelon or lemon?");
+		System.out.println("Do you prefer oranges or lemons?");
 		String fruit = sc.next();
 		
 		//Printing output based on user input
@@ -33,18 +33,30 @@ public class StringChallenge {
 			System.out.println("Ohhh sour!");
 		}
 		
-		else if(fruit.equalsIgnoreCase("WATERMELON")) {
+		//I am comparing their answer to see if it is the same as "lemons", so I can decide what to print. I have to use .equals() because my variable is a string. 
+		else if(fruit.equalsIgnoreCase("ORANGES")) {
 			System.out.println("A refreshing summer treat!");
 		}
 		
+		//I am comparing their answer to see if it is the same as "ORANGES", but it does not matter what case the letters are in. This will run as long as the letters oranges are typed. 
 		else {
 			System.out.println("Ewwwwww!");
 		}
 		
-		//Finding how much they can pay for
-		System.out.println("How much money do you have today?");
-		double money = sc.nextInt();
-		double cost = 3.45; 
+		//Calculating how many fruits they can pay for			
+		System.out.println("How many dollars do you have today?");
+		int dollars = sc.nextInt();
+		double decimalForm = dollars;
+		//I implicitly casted the dollars variable from an int to a double. Before the value would have had no decimal places, but now it does. 
+		
+		double cost = 3.25; 
+		double num = decimalForm / cost;
+		
+		int quantity = (int) num;
+		//Here I explicitly casted the num variable so that it was an int instead of a double. 
+		//This way, when I calculate how many fruits the user can buy it will be to the last whole number, not a decimal. 
+		
+		System.out.println("This is how many " + fruit + " you can buy today: " + quantity);
 		
 		sc.close();
 		
