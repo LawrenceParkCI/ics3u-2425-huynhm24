4565da0eb8b11e8879e1fb5b57b0a0724fdaf698
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2024-11-18 12:08:21 -0500

    Completed Randomness Worksheet

---
+++ ICS3U-HuynhM/src/unit2/Randomness.java
@@ -0,0 +1,28 @@
+package unit2;
+
+/**
+ * Description: This program produces a random number between 100 and 1000 
+ * Date: November 18, 2024
+ * @author Myra Huynh
+ */
+
+
+public class Randomness {
+	/**
+	 * This is the entry point to the program
+	 * @param args unused
+	 */
+  public static void main(String[] args) {
+    for (int i = 0; i < 10; i++) {
+      System.out.println((int)(Math.random()*901) + 100);
+    }
+
+    //Run the program a few times. What do you think is the 
+    //range of the numbers generated?
+    //0-1
+
+    //Read the demo program. Write the code so that the program 
+    //will produce a random number between 100 and 1000.
+    
+    }
+  }
