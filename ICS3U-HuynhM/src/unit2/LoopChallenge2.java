bead6e6870698ffba132578b9c3d3301a714fc6a
myrahuynh <myrahuynh@localhost>
2024-11-23 16:26:42 -0500

    Made changes to loop challenge 2

--- ICS3U-HuynhM/src/unit2/LoopChallenge2.java
+++ ICS3U-HuynhM/src/unit2/LoopChallenge2.java
@@ -3,7 +3,7 @@ package unit2;
 import java.util.Scanner;
 
 /**
- * Description:
+ * Description: This program finds the square root of any positive number the user inputs using a loop
  * Date: November 23, 2024
  * @author Myra Huynh 
  */
@@ -19,27 +19,27 @@ public class LoopChallenge2 {
 	      the user. Your program should use a loop to ensure that the number 
 	      they typed in is positive. If the number is negative, you should 
 	      print out some sort of warning and make them type it in again.
-	      
+
 	      You can get the square root of a number n with Math.sqrt(n). Make 
 	      sure you don't do this until the loop is done and you know for 
 	      sure you've got a positive number.
-	      
+
 	      Use the loop that you didn't use in LoopChallenge1
 		 */
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int num;
-		
-		System.out.println("Type in a number:");
-		num = sc.nextInt();	
-		
+
 		do {
-			System.out.println("Don't use a negative number!");
-			System.out.println("Type in another number:");
-			num = sc.nextInt();
+			System.out.println("Type in a number:");
+			num = sc.nextInt();	
+			
+			if (num < 0) {
+				System.out.println("Don't use a negative number!");
+			}
 		} while (num < 0);
-		
+
 		System.out.println("The square root of your number is " + Math.sqrt(num));
 
 	}
