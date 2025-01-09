39e9e4a88dd394d268c8b5d061e0ad35193f99b3
342896594 <342896594@C-MXL2393F1R.tdsb.on.ca>
2025-01-09 10:15:01 -0500

    Created arrays for category lists

--- ICS3U-HuynhM/src/culminating/MovieRoom.java
+++ ICS3U-HuynhM/src/culminating/MovieRoom.java
@@ -8,45 +8,82 @@ import java.util.Scanner;
  */
 
 public class MovieRoom {
+	
 	/**
 	 * This is the entry point to the program
 	 * @param args unused 
 	 */
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		//Declaring and initializing variables
 		int menu;
 		String movieTitle;
 		String choice;
-		String [] watchlist = new String[100]; 
 		int i = 0;
 		
-		System.out.println("Movieroom");
-		System.out.println("Welcome! What would you like to do today? Type the index number of where you want to go!");
-		System.out.println("1. Add movie");
-		System.out.println("2. Watchlist");
-		System.out.println("3. Watched");
-		System.out.println("4. Leave");
-		menu = sc.nextInt();
+		do {
+			String [] watchlist = new String[i + 1]; 
+			String [] watched = new String[100];
+			int [] rated = new int[100];
 		
-		if (menu == 1) {
-			System.out.println("Add movie");
-			System.out.println("What is the title of the movie?");
-			movieTitle = sc.nextLine();
-			System.out.println("Type + to add it to watchlist OR rate it from 1 to 5");
-			choice = sc.nextLine();
-			
-			if(choice.equals("+")) {
-				watchlist[i] = movieTitle;
-				i++;
+		
+			System.out.println("Movieroom");
+			System.out.println("Welcome! What would you like to do today? Type the index number of where you want to go!");
+			System.out.println("1. Add movie");
+			System.out.println("2. Watchlist");
+			System.out.println("3. Watched");
+			System.out.println("4. Leave");
+			menu = sc.nextInt();
+			sc.nextLine();
+
+			if (menu == 1) {
+				System.out.println("Add movie");
+				System.out.println("What is the title of the movie?");
+				movieTitle = sc.nextLine();
+				System.out.println("Type + to add it to watchlist OR rate it from 1 to 5");
+				choice = sc.next();
+
+				if(choice.equals("+")) {
+					watchlist[i] = movieTitle;
+					System.out.println(watchlist[i]);
+					i++;
+				}
+
+				else {
+					watchlist[i] = null;
+					watched[i] = movieTitle;
+					rated[i] = Integer.parseInt(choice);
+					i++;
+				}
 			}
-			
-			
-		}
+
+			if (menu == 2) {
+				System.out.println("Watchlist");
+				for (int x = watchlist.length - 1; x >= 0; x--) {
+					System.out.println(watchlist[i]);
+				}
+
+				if (menu == 3) {
+					System.out.println("Watched");
+				}
+			}
+		
+		} while (menu > 0);
 		
 		sc.close();
 	}
+	
+//	/**
+//	 * This method will print all the values of an array
+//	 * @param 
+//	 */
+//	public static String[] list(String [] listName) {
+//		
+//		for (int x = listName.length - 1; x >= 0; x--) {
+//			System.out.print(listName[x] + " ");
+//		}
+//		
+//		return listName;
+//	}
 
 }
